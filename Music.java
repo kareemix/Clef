f594881e4d874754d9c0ad2bc415c1fd4329902b
Andrew Nadar <awesomea242@gmail.com>
2024-04-30 13:14:36 -0400

    Line reader - set music object vars

---
+++ Music.java
@@ -0,0 +1,11 @@
+public class Music {
+
+    String title;
+    String composer;
+    int tempo;
+    int measures;
+    String instrument;
+    int meterNumer;
+    int meterDenom;
+
+}
--- Parser.java
+++ Parser.java
@@ -6,11 +6,69 @@ public class Parser {
             "set"
     };
 
-    public void parse(File input_file) {
+    public void parse(File inputFile) {
 
-        Scanner parseScanner = new Scanner(input_file);
+        Scanner parseScanner = new Scanner(inputFile);
 
-        int lineNumber = 1;
+        Music inputFileObject = new Music();
+
+        for (int lineNumber = 1; parseScanner.hasNextLine(); lineNumber++) {
+
+            String[] lineSplit = parseScanner.nextLine().split(" ", 2);
+
+            switch (lineSplit[0]) {
+
+                case "title":
+
+                    inputFileObject.title = lineSplit[1];
+                    break;
+
+                case "composer":
+
+                    inputFileObject.composer = lineSplit[1];
+                    break;
+
+                case "tempo":
+
+                    String tempoPreParsed = lineSplit[1];
+                    int tempo = Integer.parseInt(tempoPreParsed);
+
+                    inputFileObject.tempo = tempo;
+
+                    break;
+
+                case "meter":
+
+                    String[] meterPreParsed = lineSplit[1].split(":", 2);
+                    int meterTemp = Integer.parseInt(meterPreParsed[0]);
+
+                    inputFileObject.meterNumer = meterTemp;
+
+                    meterTemp = Integer.parseInt(meterPreParsed[1]);
+
+                    inputFileObject.meterDenom = meterTemp;
+
+                    break;
+
+                case "measures":
+
+                    String measuresPreParsed = lineSplit[1];
+                    int measuresTemp = Integer.parseInt(measuresPreParsed);
+
+                    inputFileObject.measures = measuresTemp;
+
+                    break;
+
+                default:
+
+                    System.out.println("Invalid Symbol - Line " + lineNumber + " - Symbol " + lineSplit[0]);
+                    System.exit(1);
+
+                    break;
+
+            }
+
+        }
 
     }
 
